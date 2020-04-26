c40f63ee6f763aadd5e4bbe4ff8d73a16218c6b1
Zachary Melton <zac.melton@outlook.com>
2020-04-25 19:52:53 -0500

    Started the navbar and project board components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to the Project Board</h1>
+      <h1 className="jumbotron">Welcome to the Project Board</h1>
     </div>
   );
 }
