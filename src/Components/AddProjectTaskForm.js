79ecb29673ebda386524657ce46c4f34891a5fa5
Zachary Melton <zac.melton@outlook.com>
2020-04-30 18:48:34 -0500

    Set up the state for the AddProjectTaskForm component. Created the handleChange and handleSubmit functions.

--- src/Components/AddProjectTaskForm.js
+++ src/Components/AddProjectTaskForm.js
@@ -2,6 +2,34 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class AddProjectTaskForm extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            summary: '',
+            acceptanceCriteria: '',
+            status: ''
+        };
+        this.onChange = this.handleChange.bind(this);
+        this.onSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange = (event) => {
+        this.setState({ 
+            [event.target.name]: event.target.value
+        });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const newProjectTask = {
+            summary: this.state.summary,
+            acceptanceCriteria: this.state.acceptanceCriteria,
+            status: this.state.status
+        };
+        console.log(newProjectTask);
+    }
+
     render() {
         return (
             <div className="addProjectTask">
@@ -12,32 +40,53 @@ class AddProjectTaskForm extends Component {
                             <h4 className="display-4 text-center">Add/Update Project Task</h4>
                             <hr/>
 
-                            <form>
+                            <form onSubmit={this.handleSubmit}>
+
                                 <div className="form-group">
-                                    <input type="text" className="form-control form-control-lg" name="summary" placeholder="Project Task summary" />
+                                    <input 
+                                        className="form-control form-control-lg" 
+                                        type="text"
+                                        name="summary" 
+                                        value={this.state.summary}
+                                        placeholder="Project Task summary" 
+                                        onChange={this.handleChange}
+                                    />
                                 </div>
+
                                 <div className="form-group">
-                                    <textarea className="form-control form-control-lg" placeholder="Acceptance Criteria" name="acceptanceCriteria"></textarea>
+                                    <textarea 
+                                        className="form-control form-control-lg" 
+                                        placeholder="Acceptance Criteria" 
+                                        name="acceptanceCriteria"
+                                        value={this.state.acceptanceCriteria}
+                                        onChange={this.handleChange}
+                                    ></textarea>
                                 </div>
+
                                 <div className="form-group">
-                                    <select className="form-control form-control-lg" name="status">
+                                    <select 
+                                        className="form-control form-control-lg" 
+                                        name="status"
+                                        value={this.state.status}
+                                        onChange={this.handleChange}
+                                    >
                                         <option value="">Select Status</option>
                                         <option value="TO_DO">To Do</option>
                                         <option value="IN_PROGRESS">In Progress</option>
                                         <option value="DONE">Done</option>
                                     </select>
                                 </div>
+
                                 <div className='container'>
                                     <div className='row'>
                                         <div className='col-sm-1'></div>
-                                        <Link to="/" className="btn btn-primary col-sm-4">
-                                            Back to Board
-                                        </Link>
+                                        <Link to="/" className="btn btn-primary col-sm-4">Back to Board</Link>
                                         <div className='col-sm-2'></div>
                                         <input type="submit" className="btn btn-success col-sm-4" />
                                         <div className='col-sm-1'></div>
                                     </div>
                                 </div>
+                                
                             </form>
 
                         </div>
--- src/Components/ProjectItem.js
+++ src/Components/ProjectItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 class ProjectItem extends Component {
     render() {
@@ -13,9 +14,9 @@ class ProjectItem extends Component {
                     <p className="card-text text-truncate ">
                         acceptanceCriteria
                     </p>
-                    <a href='#' className="btn btn-primary">
+                    <Link to='/addProjectTaskForm' className="btn btn-primary">
                         View / Update
-                    </a>
+                    </Link>
 
                     <button className="btn btn-danger ml-4">
                         Delete
